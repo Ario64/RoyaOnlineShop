51158b417e2c9398c9a6b79ec16ad00c76dd13e8
Ario64 <shahrokhi20@hayoo.com>
2024-11-13 15:57:01 +0330

    autocomlete added to search

---
+++ OnlineShop.Web/Controllers/ProductApiController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.DataLayer.Contexts;
+
+namespace OnlineShop.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private RoyaContext _context;
+
+        public ProductApiController(RoyaContext context)
+        {
+            _context = context;
+        }
+
+        [Produces("application/json")]
+        [HttpGet("search")]
+        public Task<IActionResult> Search()
+        {
+            try
+            {
+                string term = HttpContext.Request.Query["term"].ToString();
+
+                var productNames = _context.Products
+                    .Where(w => w.ProductName.Contains(term))
+                    .Select(s=>s.ProductName)
+                    .ToList();
+                  return Task.FromResult<IActionResult>(Ok(productNames));
+
+            }
+            catch (Exception e)
+            {
+                return Task.FromResult<IActionResult>(BadRequest());
+            }
+        } 
+    }
+}
